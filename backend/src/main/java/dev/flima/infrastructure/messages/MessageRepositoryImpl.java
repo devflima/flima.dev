0d80acfb2d79002dec4ed7ac711ad5f19fa5708f
xLima12 <felipelima1910@gmail.com>
2026-05-05 19:26:53 -0300

    infra(persistence): implement user and message adapters

---
+++ backend/src/main/java/dev/flima/infrastructure/messages/MessageRepositoryImpl.java
@@ -0,0 +1,93 @@
+package dev.flima.infrastructure.messages;
+
+import dev.flima.domain.exceptions.EntityNotFoundException;
+import dev.flima.domain.messages.Message;
+import dev.flima.domain.messages.MessageRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.NotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.UUID;
+
+@ApplicationScoped
+public class MessageRepositoryImpl implements MessageRepository, PanacheRepositoryBase<MessagePanacheEntity, UUID> {
+
+    private final ResourceBundle messages = ResourceBundle.getBundle("messages");
+    @Override
+    public void save(Message message) {
+        MessagePanacheEntity entity = new MessagePanacheEntity();
+
+        entity.id = message.getId();
+        entity.username = message.getUsername();
+        entity.email = message.getEmail();
+        entity.subject = message.getSubject();
+        entity.message = message.getMessage();
+        entity.timestamp = message.getTimestamp();
+        entity.statusMessage = message.getStatusMessage();
+
+        persist(entity);
+    }
+
+    @Override
+    public void modify(Message message) {
+        MessagePanacheEntity entity = findById(message.getId());
+
+        if (entity == null) {
+            throw new EntityNotFoundException(messages.getString("message.not_found"));
+        }
+
+        entity.username = message.getUsername();
+        entity.email = message.getEmail();
+        entity.subject = message.getSubject();
+        entity.message = message.getMessage();
+        entity.statusMessage = message.getStatusMessage();
+    }
+
+    @Override
+    public Optional<Message> getById(UUID id) {
+        MessagePanacheEntity entity = findById(id);
+
+        if (entity == null) {
+            throw new NotFoundException("Message not found");
+        }
+
+        return Optional.of(new Message(
+                entity.id,
+                entity.username,
+                entity.email,
+                entity.subject,
+                entity.message,
+                entity.timestamp,
+                entity.statusMessage
+        ));
+    }
+
+    @Override
+    public List<Message> getAll() {
+        return findAll().list().stream()
+                .map(entity -> new Message(
+                        entity.id,
+                        entity.username,
+                        entity.email,
+                        entity.subject,
+                        entity.message,
+                        entity.timestamp,
+                        entity.statusMessage
+                ))
+                .toList();
+    }
+
+    @Override
+    public void remove(Message message) {
+        MessagePanacheEntity entity = findById(message.getId());
+
+        if (entity == null) {
+            throw new EntityNotFoundException(messages.getString("message.not_found"));
+        }
+
+        delete(entity);
+    }
+}
---
+++ backend/src/main/java/dev/flima/infrastructure/users/UserRepositoryImpl.java
@@ -0,0 +1,56 @@
+package dev.flima.infrastructure.users;
+
+import dev.flima.domain.users.Password;
+import dev.flima.domain.users.User;
+import dev.flima.domain.users.UserRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.UUID;
+
+@ApplicationScoped
+public class UserRepositoryImpl implements UserRepository, PanacheRepositoryBase<UserPanacheEntity, UUID> {
+
+    private final ResourceBundle messages = ResourceBundle.getBundle("messages");
+
+    @Override
+    public long count() {
+        return PanacheRepositoryBase.super.count();
+    }
+
+    @Override
+    public void save(User user) {
+        UserPanacheEntity entity = new UserPanacheEntity();
+
+        entity.id = user.getId();
+        entity.username = user.getUsername();
+        entity.name = user.getName();
+        entity.lastName = user.getLastName();
+        entity.email = user.getEmail();
+        entity.role = user.getRole();
+        entity.password = user.getPassword().password();
+
+        persist(entity);
+    }
+
+    @Override
+    public Optional<User> getUsername(String username) {
+        UserPanacheEntity entity = find("username", username).firstResult();
+
+        if(entity == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(new User(
+                entity.id,
+                entity.username,
+                entity.name,
+                entity.lastName,
+                entity.email,
+                entity.role,
+                new Password(entity.password)
+        ));
+    }
+}
